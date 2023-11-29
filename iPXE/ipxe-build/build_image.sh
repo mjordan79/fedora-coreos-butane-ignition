0d5ae26aa7653f7ee854ae71e4d38779b0d7037c
Renato Perini <renato.perini@gmail.com>
2023-11-29 06:03:21 +0100

    Adding Matchbox chain loading

--- iPXE/ipxe-build/build_image.sh
+++ iPXE/ipxe-build/build_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# This will build the image used to build iPXE images. Used by the run.sh script.
 docker build --no-cache --force-rm -t mjordan79/ipxe-build:latest .
 
--- iPXE/ipxe-build/run.sh
+++ iPXE/ipxe-build/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+# This will build the iPXE images and it will put them in the builtimg directory.
 docker run --name ipxe-build --rm -v /$(pwd)/builtimg:/builtimg mjordan79/ipxe-build:latest
 
